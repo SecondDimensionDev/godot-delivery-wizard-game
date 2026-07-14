59293f7f7334ecd279386007680d88944ea287b8
Adam <edge1982@gmail.com>
2026-07-14 17:42:46 +0100

    Added Some Shaders

--- entities/player/player_magic_spells.gd
+++ entities/player/player_magic_spells.gd
@@ -8,13 +8,13 @@ func _process(_delta: float) -> void:
 	if current_spell == null:
 		return
 	
-	# Start casting on click
-	if Input.is_action_just_pressed("mouse_left"):
-		current_spell.start_cast()
-		
-	# Stop casting on release
-	if Input.is_action_just_released("mouse_left"):
-		current_spell.stop_cast()
+	# ONLY the local player detects their own mouse clicks
+	if player.is_multiplayer_authority():
+		if Input.is_action_just_pressed("mouse_left"):
+			rpc("_network_start_cast")
+			
+		if Input.is_action_just_released("mouse_left"):
+			rpc("_network_stop_cast")
 
 
 func _physics_process(delta: float) -> void:
@@ -28,3 +28,13 @@ func _physics_process(delta: float) -> void:
 			player.player_view_model.animation_control.blend_animation_value("IsCasting", delta, 0.0, 1.0)
 			player.animation_control.blend_animation_value("IsCasting", delta, 0.0)
 			player.animation_control.blend_animation_value("AimDirection", delta, 0.0)
+
+
+@rpc("any_peer", "call_local", "reliable")
+func _network_start_cast() -> void:
+	current_spell.start_cast()
+
+
+@rpc("any_peer", "call_local", "reliable")
+func _network_stop_cast() -> void:
+	current_spell.stop_cast()
--- scripts/game_specific_components/levitation_spell.gd
+++ scripts/game_specific_components/levitation_spell.gd
@@ -2,8 +2,21 @@ class_name LevitationSpell
 extends Spell
 
 @export var raycast: RayCast3D
+@export var beam_pivot: Node3D
 @export var lift_force: float = 15.0
 @export var player_camera: Camera3D
+
+
+# BUILT-IN VIRTUAL METHODS
+func _ready() -> void:
+	beam_pivot.hide()
+
+
+# PUBLIC FUNCTIONS
+func start_cast() -> void:
+	super()
+	beam_pivot.show()
+
 
 func process_cast(delta: float) -> void:
 	super(delta)
@@ -16,6 +29,14 @@ func process_cast(delta: float) -> void:
 		
 	if raycast.is_colliding():
 		var target = raycast.get_collider()
+		var hit_point = raycast.get_collision_point()
+		
+		# 1. Point the beam exactly at the hit object
+		beam_pivot.look_at(hit_point, Vector3.UP)
+		
+		# 2. Stretch the beam to match the distance
+		var distance = global_position.distance_to(hit_point)
+		beam_pivot.scale.z = distance
 		
 		if target is Node:
 			# Look for our custom component on the hit object
@@ -28,10 +49,20 @@ func process_cast(delta: float) -> void:
 				# Send the forces directly to whoever is simulating this box's physics
 				var authority_id = receiver.get_multiplayer_authority()
 				receiver.rpc_id(authority_id, "receive_magic_forces", force_vector, torque_vector)
+	
+	else:
+		# If we miss, maybe stretch the beam to the raycast's max length!
+		beam_pivot.scale.z = raycast.target_position.z
+		beam_pivot.rotation = Vector3.ZERO # Reset rotation to point straight forward
 
 
-## Helper function to search the hit object's children for the receiver
-func _get_spell_receiver(hit_node: Node) -> SpellReceiver:
+func stop_cast() -> void:
+	super()
+	beam_pivot.hide()
+
+
+# PRIVATE FUNCTIONS
+func _get_spell_receiver(hit_node: Node) -> SpellReceiver: # Helper function to search the hit object's children for the receiver
 	for child in hit_node.get_children():
 		if child is SpellReceiver:
 			return child
